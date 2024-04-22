375f1b2378d5f28c62fa9c99ba71de8e3ef9bcb0
343207676 <343207676@C-2UA4162GS4.tdsb.on.ca>
2024-04-22 14:57:24 -0400

    initial commit

--- src/main/java/com/mycompany/u2a1_xuricky/U2A1_Xu_Ricky.java
+++ src/main/java/com/mycompany/u2a1_xuricky/U2A1_Xu_Ricky.java
@@ -15,7 +15,9 @@ public class U2A1_Xu_Ricky extends javax.swing.JFrame {
      */
     
     ArrayList <Vehicle> vehicle = new ArrayList <Vehicle> ();
-    double gasCost, distance;
+    double gasCost, distance, pricePerPerson, efficency;
+    String license;
+    int vehicleCount = 0,numOfPassengers;
     
     public U2A1_Xu_Ricky() {
         initComponents();
@@ -40,6 +42,18 @@ public class U2A1_Xu_Ricky extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         outputRequirement = new javax.swing.JTextField();
+        jPanel2 = new javax.swing.JPanel();
+        licensePlate = new javax.swing.JTextField();
+        numPassenger = new javax.swing.JTextField();
+        farePerPerson = new javax.swing.JTextField();
+        fuelEfficency = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        submitAddVehicle = new javax.swing.JButton();
+        jScrollPane6 = new javax.swing.JScrollPane();
+        addVehicleOutput = new javax.swing.JTextArea();
         jPanel3 = new javax.swing.JPanel();
         viewVehicleNum = new javax.swing.JTextField();
         jScrollPane2 = new javax.swing.JScrollPane();
@@ -60,16 +74,6 @@ public class U2A1_Xu_Ricky extends javax.swing.JFrame {
         jLabel12 = new javax.swing.JLabel();
         jScrollPane5 = new javax.swing.JScrollPane();
         outputCompareVehicle = new javax.swing.JTextArea();
-        jPanel2 = new javax.swing.JPanel();
-        licensePlate = new javax.swing.JTextField();
-        numPassenger = new javax.swing.JTextField();
-        farePerPerson = new javax.swing.JTextField();
-        fuelEfficency = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        submitAddVehicle = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         jTextArea1.setColumns(20);
@@ -135,6 +139,81 @@ public class U2A1_Xu_Ricky extends javax.swing.JFrame {
         );
 
         jTabbedPane1.addTab("Requirements", jPanel1);
+
+        jLabel4.setText("Vehicle License Plate:");
+
+        jLabel5.setText("Number of Passengers:");
+
+        jLabel6.setText("Vehicle Fare Per Person:");
+
+        jLabel7.setText("Vehicle Fuel Efficiency (L/km):");
+
+        submitAddVehicle.setText("Submit");
+        submitAddVehicle.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                submitAddVehicleActionPerformed(evt);
+            }
+        });
+
+        addVehicleOutput.setColumns(20);
+        addVehicleOutput.setRows(5);
+        jScrollPane6.setViewportView(addVehicleOutput);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(186, 186, 186)
+                        .addComponent(submitAddVehicle))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(79, 79, 79)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, 288, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                    .addComponent(jLabel7))
+                                .addGap(50, 50, 50)
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(farePerPerson, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 96, Short.MAX_VALUE)
+                                    .addComponent(licensePlate, javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(numPassenger, javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(fuelEfficency))))))
+                .addContainerGap(68, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addComponent(licensePlate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel5)
+                    .addComponent(numPassenger, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel6)
+                    .addComponent(farePerPerson, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel7)
+                    .addComponent(fuelEfficency, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22)
+                .addComponent(submitAddVehicle)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(18, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("Add Vehicle", jPanel2);
 
         vehicleList.setEditable(false);
         vehicleList.setColumns(20);
@@ -276,66 +355,6 @@ public class U2A1_Xu_Ricky extends javax.swing.JFrame {
 
         jTabbedPane1.addTab("Compare Vehicle", jPanel4);
 
-        jLabel4.setText("Vehicle License Plate:");
-
-        jLabel5.setText("Number of Passengers:");
-
-        jLabel6.setText("Vehicle Fare Per Person:");
-
-        jLabel7.setText("Vehicle Fuel Efficiency (L/km):");
-
-        submitAddVehicle.setText("Submit");
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(79, 79, 79)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addComponent(jLabel7))
-                        .addGap(50, 50, 50)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(farePerPerson, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 96, Short.MAX_VALUE)
-                            .addComponent(licensePlate, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(numPassenger, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(fuelEfficency)))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(184, 184, 184)
-                        .addComponent(submitAddVehicle)))
-                .addContainerGap(68, Short.MAX_VALUE))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(51, 51, 51)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(licensePlate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel4))
-                .addGap(32, 32, 32)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(numPassenger, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel5))
-                .addGap(30, 30, 30)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel6)
-                    .addComponent(farePerPerson, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel7)
-                    .addComponent(fuelEfficency, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(22, 22, 22)
-                .addComponent(submitAddVehicle))
-        );
-
-        jTabbedPane1.addTab("Add Vehicle", jPanel2);
-
         jLabel1.setFont(new java.awt.Font("Sitka Text", 3, 18)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Vehicle Program");
@@ -351,9 +370,9 @@ public class U2A1_Xu_Ricky extends javax.swing.JFrame {
                         .addGap(107, 107, 107)
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 224, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(20, 20, 20)
+                        .addGap(18, 18, 18)
                         .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 452, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(24, Short.MAX_VALUE))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -376,9 +395,43 @@ public class U2A1_Xu_Ricky extends javax.swing.JFrame {
 
     private void submitRequirementsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitRequirementsActionPerformed
         // TODO add your handling code here:
-      gasCost = Double.parseDouble(gasPrice.getText());
-      distance = Double.parseDouble(distanceTravelled.getText());
+        
+        try {
+            outputRequirement.setText("");
+            gasCost = Double.parseDouble(gasPrice.getText());
+            distance = Double.parseDouble(distanceTravelled.getText());
+            
+            if (gasCost < 0 || distance < 0){
+                outputRequirement.setText("Please input positive numbers");
+                return;
+            } else
+                outputRequirement.setText("Success!");
+        } catch (Exception e) {
+            outputRequirement.setText("Please input numeric values.");
+        }
+      
     }//GEN-LAST:event_submitRequirementsActionPerformed
+
+    private void submitAddVehicleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitAddVehicleActionPerformed
+      
+       try {
+       addVehicleOutput.setText("");
+       license = licensePlate.getText();
+       numOfPassengers = Integer.parseInt(numPassenger.getText());
+       pricePerPerson = Double.parseDouble(farePerPerson.getText());
+       efficency = Double.parseDouble(fuelEfficency.getText());
+       
+       if (numOfPassengers < 0 || pricePerPerson < 0 || efficency < 0){
+           addVehicleOutput.setText("Enter positive values");
+           return;
+       }
+       Vehicle vehicle = new Vehicle (license, numOfPassengers, pricePerPerson, efficency);
+       vehicleCount++;
+       } catch (Exception e) {
+           addVehicleOutput.setText("Invalid inputs. Please check your inputs.");
+       }
+// TODO add your handling code here:
+    }//GEN-LAST:event_submitAddVehicleActionPerformed
 
     /**
      * @param args the command line arguments
@@ -416,6 +469,7 @@ public class U2A1_Xu_Ricky extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea addVehicleOutput;
     private javax.swing.JTextField distanceTravelled;
     private javax.swing.JTextField farePerPerson;
     private javax.swing.JTextField fuelEfficency;
@@ -441,6 +495,7 @@ public class U2A1_Xu_Ricky extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JScrollPane jScrollPane5;
+    private javax.swing.JScrollPane jScrollPane6;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextArea jTextArea2;
